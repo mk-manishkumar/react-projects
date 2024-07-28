da1f5e6370a1165c97c02fd24667c69d34a0aa77
Manish Kumar <manish.login01@gmail.com>
2024-07-28 22:32:07 +0530

    updated bharat clock --ver_2

--- 02_bharat_clock/src/Components/CurrentTime.jsx
+++ 02_bharat_clock/src/Components/CurrentTime.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const CurrentTime = () => {
-  let time = new Date()
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <>
       <p className="lead">
-        This is the current time : <span className='fw-bold'>{time.toLocaleDateString()}</span> - <span className='fw-bold'>{time.toLocaleTimeString()}</span>
+        This is the current time : <span className="fw-bold">{time.toLocaleDateString()}</span> - <span className="fw-bold">{time.toLocaleTimeString()}</span>
       </p>
     </>
   );
-}
+};
 
-export default CurrentTime
+export default CurrentTime;
