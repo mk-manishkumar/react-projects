89bf8cd42ae09b58a6e95aba34c8a7c2eba4e094
Manish Kumar <manish.login01@gmail.com>
2024-08-12 14:03:33 +0530

    added another project - tic tac toe

---
+++ 05_Tic_Tac_Toe/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+import TicTacToe from "./Components/TicTacToe";
+
+const App = () => {
+  return (
+    <>
+      <TicTacToe />
+    </>
+  );
+};
+
+export default App;
