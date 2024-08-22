36441372f784c448ab1b1d4f9294cd187fb6f6dd
Manish Kumar <manish.login01@gmail.com>
2024-08-22 20:09:44 +0530

    completed the real estate website project

--- 13_Real_Estate_Website/src/components/Contact/Contact.jsx
+++ 13_Real_Estate_Website/src/components/Contact/Contact.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
-
+import React from "react";
+import { RiFacebookBoxLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <section id="contact">
+        <h1>CONTACT Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi aliquid similique voluptas!</p>
+        <div className="container">
+          <img src="/about.jpg" alt="about" />
+          <div className="content">
+            <h3>Let's connect</h3>
+            <div>
+              <p>Phone</p>
+              <span>+91 1234 999</span>
+            </div>
+            <div>
+              <p>Email</p>
+              <span>mk@gmail.com</span>
+            </div>
+            <div>
+              <p>Address</p>
+              <span>House No.123 Sector A-1</span>
+            </div>
+            <ul>
+              <Link to={"/facebook"} target="_blank">
+                <RiFacebookBoxLine />
+              </Link>
+              <Link to={"/facebook"} target="_blank">
+                <RiFacebookBoxLine />
+              </Link>
+              <Link to={"/facebook"} target="_blank">
+                <RiFacebookBoxLine />
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
