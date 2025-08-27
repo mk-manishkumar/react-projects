712c01c4171d87fa9b4e9399902f79cb4cbce001
Manish Kumar <manish.login01@gmail.com>
2025-08-27 23:16:22 +0530

    feat : frontend layout complete

--- 19_Gmail_Clone/src/components/Sidebar.jsx
+++ 19_Gmail_Clone/src/components/Sidebar.jsx
@@ -1,54 +1,60 @@
-import { IoMdStar, IoMdSend } from "react-icons/io";
+import { useState } from "react";
 import { LuPencil } from "react-icons/lu";
-import { MdOutlineWatchLater, MdOutlineDrafts } from "react-icons/md";
-import { FaAngleDown } from "react-icons/fa6";
+import { IoMdStar } from "react-icons/io";
+import { MdOutlineWatchLater, MdOutlineKeyboardArrowDown, MdOutlineDrafts, MdInbox } from "react-icons/md";
+import { TbSend2 } from "react-icons/tb";
+import { useDispatch } from "react-redux";
+import { setOpen } from "../redux/appSlice";
 
 const sidebarItems = [
   {
-    icon: <LuPencil size={24} />,
+    icon: <MdInbox size={"20px"} />,
     text: "Inbox",
   },
   {
-    icon: <IoMdStar size={24} />,
+    icon: <IoMdStar size={"20px"} />,
     text: "Starred",
   },
   {
-    icon: <MdOutlineWatchLater size={24} />,
+    icon: <MdOutlineWatchLater size={"20px"} />,
     text: "Snoozed",
   },
   {
-    icon: <IoMdSend size={24} />,
+    icon: <TbSend2 size={"20px"} />,
     text: "Sent",
   },
   {
-    icon: <MdOutlineDrafts size={24} />,
+    icon: <MdOutlineDrafts size={"20px"} />,
     text: "Drafts",
-  },
-  {
-    icon: <FaAngleDown size={24} />,
-    text: "More",
   },
 ];
 
 const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
+  const dispatch = useDispatch();
   return (
     <div className="w-[15%]">
       <div className="p-3">
-        <button className="flex items-center gap-2 p-4 rounded-2xl hover:shadow bg-[#c2e7ff]">
-          <LuPencil size={24} />
+        <button onClick={() => dispatch(setOpen(true))} className="flex items-center gap-2 bg-[#C2E7FF] p-4 rounded-2xl hover:shadow-md">
+          <LuPencil size={"24px"} />
           Compose
         </button>
       </div>
       <div className="text-gray-500">
-        {sidebarItems.map((item) => {
+        {sidebarItems.map((item, idx) => {
           return (
-            <div key={item.text} className="flex items-center gap-4 pl-6 py-1 rounded-r-full hover:cursor-pointer my-2 hover:bg-gray-200">
+            <div onClick={() => setSelected(idx)} key={item.text} className={`${selected === idx ? "bg-[#C2E7FF] text-black" : "hover:bg-gray-200 hover:text-black"} flex pl-6 py-1 rounded-r-full items-center gap-4 my-2  hover:cursor-pointer`}>
               {item.icon}
               <p>{item.text}</p>
             </div>
           );
         })}
+        <div className="flex items-center pl-6 gap-4 cursor-pointer hover:bg-gray-200 rounded-r-full py-1">
+          <MdOutlineKeyboardArrowDown size={"20px"} />
+          <span>More</span>
+        </div>
       </div>
+      <div></div>
     </div>
   );
 };
