9a442c3d6f45efd9cb5291582c3780e6dfd55ecc
Manish Kumar <manish.login01@gmail.com>
2025-08-30 23:22:31 +0530

    frontend machine round project - search_product

---
+++ 20_SearchProduct/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='h-20 bg-black flex justify-center py-4'>
+      <input type="text" className='bg-white w-1/3 outline-none p-2'/>
+    </div>
+  )
+}
+
+export default Header
