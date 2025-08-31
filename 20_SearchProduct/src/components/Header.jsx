67c1177f5da209346e1b0ee69295e15e45340b24
Manish Kumar <manish.login01@gmail.com>
2025-08-31 22:51:40 +0530

    fix : fixed searchbar bug

--- 20_SearchProduct/src/components/Header.jsx
+++ 20_SearchProduct/src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Header = () => {
+  const [input, setInput] = useState("")
   return (
     <div className='h-20 bg-black flex justify-center py-4'>
-      <input type="text" className='bg-white w-1/3 outline-none p-2'/>
+      <input type="text" className='bg-white w-1/3 outline-none p-2' value={input} onChange={(e)=> setInput(e.target.value)} placeholder='Search Products...'/>
     </div>
   )
 }
