509e04d5ec98d9151d3d18b345db57044a4664f2
Manish Kumar <manish.login01@gmail.com>
2025-09-01 20:44:16 +0530

    Search_Product project completed

--- 20_SearchProduct/src/components/Header.jsx
+++ 20_SearchProduct/src/components/Header.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-
-const Header = () => {
-  const [input, setInput] = useState("")
+const Header = ({ input, setInput }) => {
   return (
-    <div className='h-20 bg-black flex justify-center py-4'>
-      <input type="text" className='bg-white w-1/3 outline-none p-2' value={input} onChange={(e)=> setInput(e.target.value)} placeholder='Search Products...'/>
+    <div className="h-20 bg-black flex justify-center py-4">
+      <input type="text" className="bg-white w-1/3 outline-none p-2" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Search Products..." />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
